5badf4d327c7845f8aa44fb7ac45095be2e44fbf
Oliver Askew <oli@aomail.com.au>
2022-01-23 11:20:05 +1100

    initialise tina and merge _app page config with previous dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
 import type { AppProps } from 'next/app'
-import { LazyMotion, domAnimation } from 'framer-motion'
+import dynamic from 'next/dynamic'
 import { globalStyles } from '../theme/global'
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
+import { LazyMotion, domAnimation } from 'framer-motion'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   globalStyles()
   return (
-    <LazyMotion features={domAnimation}>
-      <Component {...pageProps} />
-    </LazyMotion>
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
+            isLocalClient={Boolean(
+              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
+            )}
+            {...pageProps}
+          >
+            {(livePageProps: any) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <LazyMotion features={domAnimation}>
+          <Component {...pageProps} />
+        </LazyMotion>
+      </TinaEditProvider>
+    </>
   )
 }
 
-export default MyApp
+export default App
